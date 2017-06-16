400a6cea4858b6f5eec5c005837ad6c2aef396fc
zezeon <zezeon@msn.com>
2017-06-16 22:24:09 +0900

    +

--- src/corner.cpp
+++ src/corner.cpp
@@ -6,17 +6,21 @@ using namespace cv;
 
 int main(int ac, char** av)
 {
-	CVMat t = imread("ho2.jpg");
+	if(ac < 3) {
+		cout << av[0] << " [file1] [file2]" << endl;
+		return 0;
+	}
+	CVMat t = imread(av[1]);
 	t.gray();
 	t.feature<ORB>();
 	t.restore();
-	t.draw_feature();
+//	t.draw_feature();
 	t.show("corner");
-	CVMat t2 = imread("homography2.jpg");
+	CVMat t2 = imread(av[2]);
 	t2.gray();
 	t2.feature<ORB>();
 	t2.restore();
-	t2.draw_feature();
+//	t2.draw_feature();
 	t2.show("t2");
 	
 	auto matches = t.match(t2, 0.5);
---
+++ src/cut.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include"cvmatrix.h"
+#define NDEBUG
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	CVMat t = imread("AB.jpg");
+	t.gray();
+	t.filter(SOBELX);
+	t.show("corner");
+	int to_cut[t.cols] = {};
+	cout << t.cols << 'x' << t.rows << endl;
+	for(int i=0; i<t.cols; i++) for(int j=0; j<t.rows; j++) 
+		to_cut[i] += t.at<unsigned char>(j, i);
+	int* idx = max_element(to_cut, to_cut + t.cols);
+	int line = idx - to_cut;
+	t.restore();
+	CVMat a = t(Rect{0,0,line,t.rows});
+	CVMat b = t(Rect{line, 0, t.cols-line, t.rows});
+	a.show("a");
+	b.show("b");
+	imwrite("a.jpg", a);
+	imwrite("b.jpg", b);
+	waitKey(0);
+}
+
