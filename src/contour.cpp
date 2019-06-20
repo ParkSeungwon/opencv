f1444ad9dc981931a17203c4b029111eb85e1042
zeta <zezeon@msn.com>
2019-06-20 18:12:52 +0530

    business card extract

--- src/contour.cpp
+++ src/contour.cpp
@@ -1,3 +1,4 @@
+#include<algorithm>
 #include<iostream>
 #include"option.h"
 #include"cvmatrix.h"
@@ -12,19 +13,37 @@ int main(int ac, char** av)
 	};
 	if(!co.args(ac, av)) return 0;
 	CVMat t = imread(co.get<const char*>("file"));
+	t.show("original");
 	t.save();
 	t.gray();
+	t.filter(GAUSSIAN);
 	t.edge();
 	t.show();
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierachy;
 	findContours(t, contours, hierachy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 	t.restore();
-	for(auto a : contours) for(auto b : a) {
-		circle(t, b, 5, {0,0,255}, 2, 8, 0);
-		t.show("corner");
-		waitKey(1);
+	namedWindow("corner", WINDOW_NORMAL);
+	auto it = max_element(contours.begin(), contours.end(),
+			[](const vector<Point> &a, const vector<Point> &b) {
+				return a.size() < b.size();
+			}
+		);
+	Point2f xy[4] = {{1000,1000}, {0, 1000}, {1000, 0}, {0,0}};
+	for(auto &[x, y] : *it) {
+		if(x + y < xy[0].x + xy[0].y) xy[0] = {x, y};
+		if(x - y > xy[1].x - xy[1].y) xy[1] = {x, y};
+		if(y - x > xy[2].y - xy[2].x) xy[2] = {x, y};
+		if(x + y > xy[3].x + xy[3].y) xy[3] = {x, y};
 	}
+	for(auto &[x, y] : xy) cout << x << ' ' << y << endl;
+//	for(auto a : contours) {
+//		for(auto b : a) circle(t, b, 5, {0,0,255}, 2, 8, 0);
+//		t.show("corner");
+//		resizeWindow("corner", 600, 400);
+//	}
+	t.get_business_card(xy);
+	t.show("final");
 	waitKey(0);
 }
 
--- src/detectline.cpp
+++ src/detectline.cpp
@@ -3,20 +3,36 @@
 using namespace std;
 using namespace cv;
 
+CVMat t;
+int thres = 70, cont = 100, hop = 10, scale = 10;
+void callback(int, void*) {
+	t.restore();
+	t.gray();
+	t.filter(GAUSSIAN);
+	t.edge();
+	t.detect_line(thres, cont, hop);
+	t.restore();
+	t.draw_detected_line();
+	t.scale(scale * 0.1, scale * 0.1);
+	t.show("line");
+}
 int main(int ac, char** av)
 {
 	if(ac < 2) return 0;
-	CVMat t = imread(av[1]);
+	t = imread(av[1]);
+	t.save();
 	t.gray();
 	t.filter(GAUSSIAN);
-	t.detect_circle();
 	t.edge();
-	t.detect_line();
 	t.show("edge");
+	t.detect_line(70, 100, 10);
 	t.restore();
 	t.draw_detected_line();
-	t.draw_detected_circle();
 	t.show("line");
+	createTrackbar("hop", "line", &hop, 100, callback);
+	createTrackbar("thres", "line", &thres, 300, callback);
+	createTrackbar("cont", "line", &cont, 300, callback);
+	createTrackbar("scale", "line", &scale, 10, callback);
 	waitKey(0);
-}
+}//48 88 130
 
